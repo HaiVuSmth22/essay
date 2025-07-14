c5f3a59c8b324e2d71b52e970027a98ba96813f3
HaiVuSmth22 <haivugiangthai@gmail.com>
2025-07-14 13:18:25 +0700

    sua lai ti

--- myScript.js
+++ myScript.js
@@ -29,7 +29,7 @@ function updateCartDisplay() {
     cartTotalEl.textContent = "Total: $0.00";
     return;
   }
-
+//Change quantity in cart and calculate total
   cart.forEach((item, index) => {
     const itemTotal = item.price * item.quantity;
     total += itemTotal;
@@ -71,6 +71,7 @@ function removeItem(index) {
   cart.splice(index, 1);
   localStorage.setItem("cart", JSON.stringify(cart));
   updateCartDisplay();
+  updateCartCount();
 }
 
 // Update cart icon count
